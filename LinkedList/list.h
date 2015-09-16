ad6d97e47099f52af7cfb50138bd27726afd58b3
friendlysipayung <friendly.1301144001@gmail.com>
2015-09-16 17:53:06 +0700

    list.h
    Friendly Halomoan Sipayung
    1301144001
    Bagian:
    -Insert After
    -Delete After
    -Insertion Sort

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,27 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+struct infotype
+{
+    char no_hp[14], nama [20], alamat[100],email[50];
+    int ID_customer;
+};
 
+typedef struct elmlist *address;
+
+struct elmlist
+{
+    infotype info;
+    address next;
+};
+
+struct list
+{
+    address first;
+};
+
+void InsertAfter (list *l,address q, address p);
+void deleteAfter (list *l,address q, address p);
+void insertionsort(list l);
 
 #endif // LIST_H_INCLUDED
